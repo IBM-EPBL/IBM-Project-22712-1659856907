9096827a41447dab42ff92a947285e4ca7acdcbf
Amrita1107 <68951622+Amrita1107@users.noreply.github.com>
2022-11-17 10:26:06 +0530

    Add files via upload

---
+++ PROJECT DEVELOPMENT PHASE/SPRINT 3/patient/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class PatientUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
+        widgets = {
+            'password': forms.PasswordInput()
+        }
+
+
+class PatientForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Patient
+        fields = ['age', 'bloodgroup', 'disease',
+                  'doctorname', 'address', 'mobile', 'profile_pic']
